0f08ab1780c1d200a742300842fd9cd5b5f3eedf
Marcos Andrade <marcos_q_andrade@hotmail.com>
2022-07-31 12:59:35 -0300

    Category

--- src/main/java/com/example/demo/course/config/TestConfig.java
+++ src/main/java/com/example/demo/course/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.example.demo.course.entities.Category;
 import com.example.demo.course.entities.Order;
 import com.example.demo.course.entities.User;
 import com.example.demo.course.enums.OrderStatus;
+import com.example.demo.course.repositories.CategoryRepository;
 import com.example.demo.course.repositories.OrderRepository;
 import com.example.demo.course.repositories.UserRepository;
 
@@ -23,6 +25,9 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private OrderRepository orderRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -33,8 +38,13 @@ public class TestConfig implements CommandLineRunner {
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),OrderStatus.WAITING_PAYMENT, u2);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),OrderStatus.WAITING_PAYMENT, u1);
 		
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers");
+		
 		userRepository.saveAll(List.of(u1,u2));
 		orderRepository.saveAll(List.of(o1,o2,o3));
+		categoryRepository.saveAll(List.of(cat1,cat2,cat3));
 	}
 	
 	
