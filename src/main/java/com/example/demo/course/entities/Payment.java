3cd40e6af70b71ce810ce7696449e5030440fc57
Marcos Andrade <marcos_q_andrade@hotmail.com>
2022-08-01 15:05:28 -0300

    Subtotal & Total methods

--- src/main/java/com/example/demo/course/entities/Payment.java
+++ src/main/java/com/example/demo/course/entities/Payment.java
@@ -1,5 +1,7 @@
 package com.example.demo.course.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 import java.io.Serial;
 import java.io.Serializable;
@@ -15,6 +17,7 @@ public class Payment implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long Id;
     private Instant moment;
+    @JsonIgnore
     @OneToOne
     @MapsId
     private Order order;
