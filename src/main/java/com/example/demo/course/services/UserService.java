1ec81f1dab86a452727526096e54f3e792e87e0e
Marcos Andrade <marcos_q_andrade@hotmail.com>
2022-08-02 11:01:11 -0300

    Exception gandling - findById

--- src/main/java/com/example/demo/course/services/UserService.java
+++ src/main/java/com/example/demo/course/services/UserService.java
@@ -2,10 +2,10 @@ package com.example.demo.course.services;
 
 import com.example.demo.course.entities.User;
 import com.example.demo.course.repositories.UserRepository;
+import com.example.demo.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.persistence.EntityNotFoundException;
 import java.util.List;
 
 @Service
@@ -21,7 +21,7 @@ public class UserService {
 	public User findById(Long id) {
 		
 		return userRepository.findById(id).orElseThrow(
-				() -> new EntityNotFoundException("User not found"));
+				() -> new ResourceNotFoundException(id));
 	}
 
 	public User insert(User obj){
